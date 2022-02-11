29fac16fa456404ce7ceb1cf9738b4cdf7be09f3
hungphuoc209 <nguyenhungphuoc209@gmail.com>
2022-02-12 01:15:56 +0700

    done

--- app/src/main/java/com/example/weather/ultils/Constant.java
+++ app/src/main/java/com/example/weather/ultils/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
     public static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
     public static final String ICON_BASE_URL = "https://openweathermap.org/img/wn/";
     public static final String ICON_SUB_PARAM = "@2x.png";
-    public static final String API_KEY = "33bc2dcb90af984be651ec3436b23436";
+    public static final String API_KEY = "70889762a57654c1274f56b8326c7a5a";
     public static final String QUERY_CITY = "q";
     public static final String QUERY_KEY = "appid";
     public static final String CURRENT_WEATHER = "weather";
